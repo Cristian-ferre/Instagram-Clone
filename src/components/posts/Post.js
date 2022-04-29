9dfdf0a849a1a3551c3091f127cd4db97686dad5
Cristian ferreira <cristianferreiradeoliveira.ti@gmail.com>
2022-04-28 23:47:55 -0300

    first commit

---
+++ src/components/posts/Post.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Post.css";
+import Like from "./images/like.png"
+
+function Post({username, caption, imageUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <h3>{username}</h3>
+      </div>
+      {/*  */}
+      <img className="post__image" src={imageUrl} alt={caption} title={caption} />
+      {/* */}
+      <h4 className="post__text">
+        <strong>{username}</strong> {caption}
+      </h4>    
+      <div >
+        <img src={Like} />
+      </div>
+    </div>
+  );
+}
+
+export default Post;
